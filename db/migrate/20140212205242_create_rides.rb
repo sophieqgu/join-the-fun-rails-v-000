3774cac9954c8c40f37b293b4a046f99b029ad2e
Sophie Gu <sophieqgu@gmail.com>
2020-12-02 06:19:44 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,8 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-      t.index :taxi_id
-      t.index :passenger_id
+      t.integer :taxi_id
+      t.integer :passenger_id
       t.timestamps null: false
     end
   end
