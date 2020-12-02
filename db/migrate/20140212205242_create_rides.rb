90bcefce935b01a9f0aae3124447eca6a5cf37a0
Sophie Gu <sophieqgu@gmail.com>
2020-12-02 06:06:26 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,7 @@
 class CreateRides < ActiveRecord::Migration
   def change
-    create_table :rides do |t|
+    create_join_table :rides do |t|
+      t.index
       t.timestamps null: false
     end
   end
